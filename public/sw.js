33565b37ad1ffd913f1cdd57775d0d6936b1b576
xingchenyd <275875933+xingchenyd@users.noreply.github.com>
2026-09-01 18:00:21 +0800

    fix: refresh PWA navigation cache on deploy

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,33 @@
-const CACHE = 's-game-v2'
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './manifest.webmanifest']))))
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))))
+const CACHE = 's-game-v3'
+const SHELL = ['./', './manifest.webmanifest']
+
+self.addEventListener('install', event => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim(),
+  ]))
+})
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone()
+      caches.open(CACHE).then(cache => cache.put('./', copy))
+      return response
+    }).catch(() => caches.match('./')))
+    return
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone()
-    caches.open(CACHE).then(cache => cache.put(event.request, copy))
+    if (response.ok) {
+      const copy = response.clone()
+      caches.open(CACHE).then(cache => cache.put(event.request, copy))
+    }
     return response
-  }).catch(() => caches.match('./'))))
+  })))
 })
